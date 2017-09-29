08f1e078551c4d69255a00614eaaba5a8ec6266d
Nate Keefe <nathan.keefe@gmail.com>
2017-09-29 16:44:24 -0700

    added fields

--- Objects/IncomingMessages.cs
+++ Objects/IncomingMessages.cs
@@ -25,7 +25,26 @@ namespace CDK.Objects.IncomingMessages
         public string DateTime { get; set; }
         public string Email { get; set; }
         public string Int { get; set; }
-        public string String { get; set; }
+        public string String1 { get; set; }
+        public string String2 { get; set; }
+        public string String3 { get; set; }
+        public string String4 { get; set; }
+        public string String5 { get; set; }
+        public string String6 { get; set; }
+        public string String7 { get; set; }
+        public string String8 { get; set; }
+        public string String9 { get; set; }
+        public string String10 { get; set; }
+        public string String11 { get; set; }
+        public string String12 { get; set; }
+        public string String13 { get; set; }
+        public string String14 { get; set; }
+        public string String15 { get; set; }
+        public string String16 { get; set; }
+        public string String17 { get; set; }
+        public string String18 { get; set; }
+        public string String19 { get; set; }
+        public string String20 { get; set; }
     }
 
 }
--- Objects/OutgoingMessage.cs
+++ Objects/OutgoingMessage.cs
@@ -8,13 +8,32 @@ namespace CDK.Objects.Messages
 {
     public class Message
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
         public string Id { get; set; }
-        public string AccountNumber { get; set; }
+        public string Name { get; set; }
         public string Account { get; set; }
+        public string AccountNumber { get; set; }
         public string DateTime { get; set; }
+        public string Email { get; set; }
         public string Int { get; set; }
-        public string String { get; set; }
+        public string String1 { get; set; }
+        public string String2 { get; set; }
+        public string String3 { get; set; }
+        public string String4 { get; set; }
+        public string String5 { get; set; }
+        public string String6 { get; set; }
+        public string String7 { get; set; }
+        public string String8 { get; set; }
+        public string String9 { get; set; }
+        public string String10 { get; set; }
+        public string String11 { get; set; }
+        public string String12 { get; set; }
+        public string String13 { get; set; }
+        public string String14 { get; set; }
+        public string String15 { get; set; }
+        public string String16 { get; set; }
+        public string String17 { get; set; }
+        public string String18 { get; set; }
+        public string String19 { get; set; }
+        public string String20 { get; set; }
     }
 }
